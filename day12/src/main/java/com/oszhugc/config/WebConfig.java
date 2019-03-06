adb93378614dd39b424a634f3bc85bcae7c2d47d
oszhugc <1715694655@qq.com>
2019-03-06 23:08:12 +0800

    ss

--- day12/src/main/java/com/oszhugc/config/WebConfig.java
+++ day12/src/main/java/com/oszhugc/config/WebConfig.java
@@ -1,7 +1,21 @@
 package com.oszhugc.config;
 
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 /**
  * Created by Administrator on 2019\3\6 0006.
  */
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        //使得"/login/page"映射为"login.jsp"
+        registry.addViewController("/login/page").setViewName("login");
+        //使得/logout/page映射为logout_welcome.jsp
+        registry.addViewController("/logout/page").setViewName("logout_welcome");
+        //使得/logout映射logout.jsp
+        registry.addViewController("/logout").setViewName("logout");
+    }
+
 }
--- day12/src/main/java/com/oszhugc/pojo/DatabaseRole.java
+++ day12/src/main/java/com/oszhugc/pojo/DatabaseRole.java
@@ -1,10 +1,17 @@
 package com.oszhugc.pojo;
 
-import org.apache.ibatis.type.Alias;
-
 import java.io.Serializable;
 
+import org.apache.ibatis.type.Alias;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 @Alias("dbrole")
+@Getter
+@Setter
+@AllArgsConstructor
 public class DatabaseRole implements Serializable {
 
     private Long id;
